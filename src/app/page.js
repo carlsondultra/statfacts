eedd22bfd0a99a559afab9a8a7791c3d2876cd28
carlsondultra <carlsondultra@users.noreply.github.com>
2023-10-28 18:24:53 -0400

    displaying team name on front-end

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,19 @@
 
-async function getTeams(){
-  const response = await fetch('https://www.balldontlie.io/api/v1/teams')
+async function getTeams() {
+  const response = await fetch('https://www.balldontlie.io/api/v1/teams/28')
   const data = await response.json()
   return data
 }
 
 export default async function Home() {
-
   const teams = await getTeams()
   console.log(teams)
+
   return (
-   <div className="text-center mt-20">
+    <div className="text-center mt-20">
       <h1 className="text-5xl font-bold">Home</h1>
-   </div>
+      <h1>{teams.full_name}</h1>
+      <h1>{teams.conference}</h1>
+    </div>
   )
 }
