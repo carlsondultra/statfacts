98df4119679c08aaabc8b3d9d699c8658eb76a35
carlsondultra <carlsondultra@users.noreply.github.com>
2023-10-28 18:05:38 -0400

    fetching NBA teams displaying response in console

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
-import Image from 'next/image'
 
-export default function Home() {
+async function getTeams(){
+  const response = await fetch('https://www.balldontlie.io/api/v1/teams')
+  const data = await response.json()
+  return data
+}
+
+export default async function Home() {
+
+  const teams = await getTeams()
+  console.log(teams)
   return (
-   <div>
-    
+   <div className="text-center mt-20">
+      <h1 className="text-5xl font-bold">Home</h1>
    </div>
   )
 }
