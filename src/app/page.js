aa13466acc575cfe883bdf533a726e8689dd4718
carlsondultra <carlsondultra@users.noreply.github.com>
2023-10-30 16:20:42 -0400

    mapping list of players and their respsecitve teams

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,25 @@
 async function getPlayer() {
-  const response = await fetch('https://www.balldontlie.io/api/v1/players?search=lebron')
+  const response = await fetch('https://www.balldontlie.io/api/v1/players')
   const data = await response.json()
   return data
 }
-
 export default async function Home() {
   
   const players = await getPlayer()
-  console.log(players)
-  console.log(players.data[0].team)
+  console.log(players.data)
+
+  let mappedArray = (players.data).map((players) => {
+    return(
+      <li>{players.first_name} {players.last_name} - {players.team.full_name}</li>
+    )
+  })
 
   return (
     <div className="text-center mt-20">
-      <h1 className="text-5xl font-bold">Players</h1><br></br>
-      <h1>Name: {players.data[0].first_name} {players.data[0].last_name}</h1>
-      <h1>Height: {players.data[0].height_feet}'{players.data[0].height_inches}</h1>
-      <h1>Plays for: {players.data[0].team.full_name} </h1>
+      <h1 className="text-5xl font-bold">statfacts</h1>
+      <ul>
+        {mappedArray}
+      </ul>
     </div>
   )
 }
