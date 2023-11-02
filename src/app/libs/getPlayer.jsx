26d212bd894b0fb3f31c65c6d14ada8d03db78ea
carlsondultra <carlsondultra@users.noreply.github.com>
2023-11-02 15:38:31 -0400

    changing response error display

--- src/app/libs/getPlayer.jsx
+++ src/app/libs/getPlayer.jsx
@@ -2,7 +2,7 @@ export default async function getPlayer(id) {
     const response = await fetch(`https://www.balldontlie.io/api/v1/players/${id}`)
 
     if(!response.ok) {
-        throw new Error('failed to fetch user')
+        throw new Error('failed to fetch players')
     }
 
     return response.json()
--- src/app/libs/getPlayers.jsx
+++ src/app/libs/getPlayers.jsx
@@ -2,7 +2,7 @@ export default async function getPlayers() {
     const response = await fetch('GET https://www.balldontlie.io/api/v1/players')
 
     if(!response.ok) {
-        throw new Error('failed to fetch users')
+        throw new Error('failed to fetch players')
     }
 
     return await response.json()
