de1e65afbb81b7a344517554e54d8c6c812bba47
carlsondultra <carlsondultra@users.noreply.github.com>
2023-11-02 16:55:10 -0400

    fixing url for player retrieval

--- src/app/libs/getPlayers.jsx
+++ src/app/libs/getPlayers.jsx
@@ -1,5 +1,5 @@
 export default async function getPlayers() {
-    const response = await fetch('GET https://www.balldontlie.io/api/v1/players')
+    const response = await fetch('https://www.balldontlie.io/api/v1/players')
 
     if(!response.ok) {
         throw new Error('failed to fetch players')
