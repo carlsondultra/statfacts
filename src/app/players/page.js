37e1e36045fce440dfcd6aad5721aafd41b351bd
carlsondultra <carlsondultra@users.noreply.github.com>
2023-11-04 21:16:17 -0400

    adding drop down menu with list of players

--- src/app/players/page.js
+++ src/app/players/page.js
@@ -5,18 +5,36 @@ export default async function PlayersPage() {
     const players = await getPlayers()
     return (
         <div className="text-center mt-20">
-            <h1 className="text-5xl font-bold">players</h1> 
+            <h1 className="text-5xl font-bold">players</h1>
+            {/* drop down menu with list of players */}
+            <select>
+                <option value="Choose a cat">Choose a player</option>
+                {players.data.map(player => {
+                    return (
+                        <>
+                            <option>
+                                <p key={player.id}>
+                                    <Link href={`/players/${player.id}`}>
+                                        {player.first_name} {player.last_name}
+                                    </Link>
+                                </p>
+                            </option>
+                        </>
+                    )
+                })
+                }
+            </select>
             {players.data.map(player => {
                 return (
                     <>
-                    <p key={player.id}>
-                        <Link href={`/players/${player.id}`}>
-                            {player.first_name} {player.last_name}
-                        </Link>
-                    </p>
+                        <p key={player.id}>
+                            <Link href={`/players/${player.id}`}>
+                                {player.first_name} {player.last_name}
+                            </Link>
+                        </p>
                     </>
                 )
-                })
+            })
             }
 
         </div>
