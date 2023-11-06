341f9911fec60e6ece720d71394b0f20e46e150f
carlsondultra <carlsondultra@users.noreply.github.com>
2023-11-05 20:59:16 -0500

    fetching client-side instead of server-side

--- src/app/players/page.js
+++ src/app/players/page.js
@@ -1,15 +1,31 @@
+'use client'
 import Link from "next/link"
 import getPlayers from "../libs/getPlayers"
+import React, { useState, useEffect } from "react"
 
-export default async function PlayersPage() {
-    const players = await getPlayers()
+export default function PlayersPage() {
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch('https://www.balldontlie.io/api/v1/players')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data)
+                setLoading(false)
+                console.log(data)
+            })
+    }, [])
+
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No profile data</p>
+
     return (
         <div className="text-center mt-20">
             <h1 className="text-5xl font-bold">players</h1>
             {/* drop down menu with list of players */}
             <select>
-                <option value="Choose a cat">Choose a player</option>
-                {players.data.map(player => {
+                {data.data.map(player => {
                     return (
                         <>
                             <option>
@@ -24,19 +40,6 @@ export default async function PlayersPage() {
                 })
                 }
             </select>
-            {players.data.map(player => {
-                return (
-                    <>
-                        <p key={player.id}>
-                            <Link href={`/players/${player.id}`}>
-                                {player.first_name} {player.last_name}
-                            </Link>
-                        </p>
-                    </>
-                )
-            })
-            }
-
         </div>
     )
 }
